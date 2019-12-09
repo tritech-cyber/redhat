a6cc240c2eb27ca3cd2eaa533815d25d849aa96f
cwc <cwc@localhost.localdomain>
2019-12-09 05:01:30 -0800

    AA

--- setip.sh
+++ setip.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-nmcli con mod enp0s3 ipv4.addresses 192.168.1.13/24;
+# type ip a first then check tate the nic label is enp0s3
+# set your assigned ip then run this script
+# use 10.40.0.14 for the dns server if 8.8.8.8 does not work
+nmcli con mod enp0s3 ipv4.addresses 192.168.1.17/24;
 nmcli con mod enp0s3 ipv4.gateway 192.168.1.1;
 nmcli con mod enp0s3 ipv4.method manual;
 nmcli con mod enp0s3 ipv4.dns "8.8.8.8";
