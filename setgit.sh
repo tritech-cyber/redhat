8d0394890484566abe4d07bd9a54fa652e9248eb
icebowl <icebowl1967@gmail.com>
2019-12-10 04:35:42 -0800

    AA

---
+++ setgit.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username yourgithubusername;
+git config --global user.name yourgithubusername;
+git config --global user.user yourgithubusername;
+git config --global user.email yourgithubemail@mail.ioe;
+git config --list;
