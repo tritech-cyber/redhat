c07f8a81edc6d486ca6fbce7f0556df36722ed90
icebowl <icebowl1967@gmail.com>
2020-05-01 06:35:16 -0700

    AA

---
+++ tcpdmp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+now=$(date +"%Y%m%d-%H%M%S")
+echo `pwd`
+nowfile=$(date +"%Y%m%d-%H%M%S")
+#nohup /usr/sbin/tcpdump -w `pwd`/$nowfile.pcap -i enp2s0  >> /dev/null 2>&1 & 
+nohup /usr/sbin/tcpdump  > `pwd`/$now.txt &
+#now=$(date +"%Y%m%d-%H%M%S")
+# #printf "Today we are going to backup file to NFS server at AWS '%s'\n" "/efs/my-blog-${today}.sql.tar.gz"
+#nowfile=$(date +"%Y%m%d-%H%M%S")
+#nohup tcpdump -w $(date +"%Y%m%d-%H%M%S").pcap -i enp2s0  >> /dev/null 2>&1 & 
